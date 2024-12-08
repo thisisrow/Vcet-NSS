a2f9955dd1a890f7d1cff5a8c88f7c69da6dc040
thisisrow <prathameshmishra2020@gmail.com>
2024-12-08 21:49:37 +0530

    context update

--- nss/context/authContext.js
+++ nss/context/authContext.js
@@ -27,7 +27,7 @@ const AuthProvider = ({ children }) => {
 
   //default axios setting
   axios.defaults.headers.common["Authorization"] = `Bearer ${token}`;
-  axios.defaults.baseURL = "http://192.168.38.117:8081/api/v1";
+  axios.defaults.baseURL = "https://post-server-owes.onrender.com";
 
   return (
     <AuthContext.Provider value={[state, setState]}>
